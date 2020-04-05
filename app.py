418909687d9edbfde89c593463ee26da4fd92c56
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2020-04-05 23:58:06 +0200

    updated tables definition + initialized REST API calls + created script to create SQLite DB

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import json
+import string
+import random
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import aliased
 
 
 app = Flask(__name__)
@@ -11,24 +14,48 @@ db = SQLAlchemy(app)
 
 
 class Person(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
-    address = db.Column(db.String)
 
-    phone_numbers = db.relationship('PhoneNumber', backref='person', lazy=True)
-    residents = db.relationship('Resident', backref='person', lazy=True)
-    contributors = db.relationship('Contributor', backref='person', lazy=True)
+    __tablename__ = 'persons'
+
+    id = db.Column(db.String, primary_key=True)
+    first_name = db.Column(db.String, index=True, nullable=False)
+    last_name = db.Column(db.String, index=True, nullable=False)
+    address = db.Column(db.String)
 
 
 class PhoneNumber(db.Model):
-    phone_number = db.Column(db.String, primary_key=True) 
+    __tablename__ = 'phone_numbers'
 
-    person_id = db.Column(db.String, db.ForeignKey('person.id'), nullable=False)
+    phone_number = db.Column(db.String, primary_key=True)
+
+    person_id = db.Column(db.String, db.ForeignKey(
+        'persons.id'), nullable=False)
+
+
+class Contributor(db.Model):
+
+    __tablename__ = 'contributors'
+
+    id = db.Column(db.String, db.ForeignKey('persons.id'), primary_key=True)
+
+    role = db.Column(db.String, nullable=True)
+
+
+class HealthMutual(db.Model):
+
+    __tablename__ = 'health_mutuals'
+
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, index=True)
+    address = db.Column(db.String, index=True)
+    phone_number = db.Column(db.String)
 
 
 class Resident(db.Model):
-    id = db.Column(db.String, db.ForeignKey('person.id'), primary_key=True)
+
+    __tablename__ = 'residents'
+
+    id = db.Column(db.String, db.ForeignKey('persons.id'), primary_key=True)
 
     birth_date = db.Column(db.Date)
     birthplace = db.Column(db.String)
@@ -36,54 +63,98 @@ class Resident(db.Model):
     emergency_bag = db.Column(db.String)
     social_welfare_number = db.Column(db.String)
 
-    health_mutual_id = db.Column(db.String, db.ForeignKey('health_mutual.id'), nullable=True)
-    referring_doctor_person_id = db.Column(db.String, db.ForeignKey('person.id'), nullable=True)
-    psychiatrist_person_id = db.Column(db.String, db.ForeignKey('person.id'), nullable=True)
-    
+    health_mutual_id = db.Column(
+        db.String, db.ForeignKey('health_mutuals.id'), nullable=True)
+    referring_doctor_id = db.Column(
+        db.String, db.ForeignKey('persons.id'), nullable=True)
+    psychiatrist_id = db.Column(
+        db.String, db.ForeignKey('persons.id'), nullable=True)
 
-class Contributor(db.Model):
-    id = db.Column(db.String, db.ForeignKey('person.id'), primary_key=True)
-
-    role = db.Column(db.String, nullable=True)
-
-
-class HealthMutual(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String)
-    address = db.Column(db.String)
-    phone_number = db.Column(db.String)
-
-    residents = db.relationship('Resident', backref='health_mutual', lazy=True)
+    resident = db.relationship('Person', foreign_keys=[id])
+    referring_doctor = db.relationship(
+        'Person', foreign_keys=[referring_doctor_id])
+    psychiatrist = db.relationship('Person', foreign_keys=[psychiatrist_id])
 
 
 emergency_relationships = db.Table('emergency_relationships',
-    db.Column('resident_id', db.String, db.ForeignKey('resident.id'), primary_key=True),
-    db.Column('person_id', db.String, db.ForeignKey('person.id'), primary_key=True),
-    db.Column('relationship', db.String, primary_key=True)
-)
+                                   db.Column('resident_id', db.String, db.ForeignKey(
+                                       'residents.id'), primary_key=True),
+                                   db.Column('person_id', db.String, db.ForeignKey(
+                                       'persons.id'), primary_key=True),
+                                   db.Column('relationship',
+                                             db.String, primary_key=True)
+                                   )
 
 
 contribution_relationships = db.Table('contribution_relationships',
-    db.Column('resident_id', db.String, db.ForeignKey('resident.id'), primary_key=True),
-    db.Column('contributor_id', db.String, db.ForeignKey('contributor.id'), primary_key=True),
-    db.Column('social_advising_relationship', db.Boolean, primary_key=True)
-)
+                                      db.Column('resident_id', db.String, db.ForeignKey(
+                                          'residents.id'), primary_key=True),
+                                      db.Column('contributor_id', db.String, db.ForeignKey(
+                                          'contributors.id'), primary_key=True),
+                                      db.Column(
+                                          'social_advising_relationship', db.Boolean, primary_key=True)
+                                      )
 
+
+def get_random_id(size: int) -> str:
+    random.seed()
+    charset = string.digits + string.ascii_letters + '_'
+
+    generated_id = ''
+
+    for _ in range(0, size):
+        generated_id = generated_id + \
+            charset[random.randint(0, len(charset) - 1)]
+
+    return generated_id
 
 
 @app.route('/residents', methods=['GET', 'POST'])
 def residents_collection():
-    return None
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        # extract the information
+        payload = request.json
+
+        resident_id = get_random_id(8)
+
+        person = Person(
+            id=resident_id,
+            first_name=payload.get('firstName'),
+            last_name=payload.get('lastName'),
+            address=payload.get('address')
+        )
+
+        db.session.add(person)
+
+        resident = Resident(
+            id=resident_id,
+            birth_date=payload.get('birthDate'),
+            birthplace=payload.get('birthplace'),
+            entrance_date=payload.get('entranceDate'),
+            emergency_bag=payload.get('emergencyBag'),
+            social_welfare_number=payload.get('socialWelfareNumber'),
+            health_mutual_id=payload.get('healthMutualId'),
+            referring_doctor_id=payload.get('referringDoctorId'),
+            psychiatrist_id=payload.get('psychiatristId')
+        )
+
+        db.session.add(resident)
+        db.session.commit()
+
+    return json.dumps({'id': resident_id})
+
 
 @app.route('/residents/<string:resident_id>', methods=['GET', 'PATCH', 'PUT', 'DELETE'])
 def resident_item(resident_id):
-    return None
+    if request.method == 'GET':
+        query = db.session.query(Person, Resident, PhoneNumber, HealthMutual, emergency_relationships, contribution_relationships) \
+            .filter(Person.id == resident_id) \
+            .join(Resident, Resident.id == Person.id) \
+            .outerjoin(PhoneNumber, PhoneNumber.person_id == Person.id) \
+            .outerjoin(HealthMutual, HealthMutual.id == Resident.health_mutual_id) \
+            .outerjoin(emergency_relationships, emergency_relationships.resident_id == Resident.id) \
+            .outerjoin(contribution_relationships, contribution_relationships.resident_id == Resident.id)
 
-
-@app.route('/health-mutuals', methods=['GET', 'POST'])
-def health_mutuals_collection():
-    return None
-
-@app.route('/health-mutuals/<string:health_mutual_id>', methods=['GET', 'PATCH', 'PUT', 'DELETE'])
-def health_mutual_item(health_mutual_id):
     return None
