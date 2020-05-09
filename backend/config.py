762c2aa5cdacc86dfb538d253013d8fca9c35856
micamath <michau.mathilde@gmail.com>
2020-05-09 21:44:49 +0200

    change location pf db, and git ignore it

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # ...
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.db')
+        'sqlite:///' + os.path.join(basedir, 'db_instances', 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
