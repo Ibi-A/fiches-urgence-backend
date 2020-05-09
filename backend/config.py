3cbf10e48eeeef8c7541584a96c10dd977c2629d
micamath <michau.mathilde@gmail.com>
2020-05-09 23:03:00 +0200

    add Config for testing purposes

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'db_instances', 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ConfigTest:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'db_instances', 'test.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
