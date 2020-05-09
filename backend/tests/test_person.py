e6f2f289a983e13affe0742907b0c3a6db0bf9b4
micamath <michau.mathilde@gmail.com>
2020-05-09 23:13:40 +0200

    add tests for person and extract default Person object

--- backend/tests/test_person.py
+++ backend/tests/test_person.py
@@ -1,6 +1,6 @@
 from backend.models import Person
 from backend import db
-from config_test import TestWebsite, client
+from config_test import TestApi, client
 from nose.tools import eq_, ok_
 
 #   ___ ___ ___  ___  ___  _  _
@@ -8,8 +8,16 @@ from nose.tools import eq_, ok_
 #  |  _/ _||   /\__ \ (_) | .` |
 #  |_| |___|_|_\|___/\___/|_|\_|
 
+PERSON = {
+    "firstName": "name",
+    "lastName": "name",
+    "address": "address",
+    'alternativePhoneNumber': "altPhone",
+    'mainPhoneNumber': "mainPhone"
+}
 
-class TestPerson(TestWebsite):
+
+class TestPerson(TestApi):
 
     def test_get_persons(self):
         res = client.get('/persons')
@@ -20,32 +28,31 @@ class TestPerson(TestWebsite):
         eq_(400, res.status_code)
 
     def test_post_persons(self):
-        person = {
-            "firstName": "name",
-            "lastName": "name",
-            "address": "address",
-            'alternativePhoneNumber': "altPhone",
-            'mainPhoneNumber': "mainPhone"
-        }
-        res = client.post('/persons', json=person)
+        res = client.post('/persons', json=PERSON)
 
         eq_(201, res.status_code)
-        person["id"] = res.json["id"]
+        PERSON["id"] = res.json["id"]
+        eq_(PERSON, res.json)
 
-        res = client.get(f'/persons/{person["id"]}')
+        res = client.get(f'/persons/{PERSON["id"]}')
         eq_(200, res.status_code)
-        eq_(person, res.json)
+        eq_(PERSON, res.json)
 
-    def test_post_persons(self):
-        person = {
-            "firstName": "name",
-            "lastName": "name",
-            "address": "address",
-            'alternativePhoneNumber': "altPhone",
-            'mainPhoneNumber': "mainPhone"
+    def test_put_person(self):
+        res_post = client.post('/persons', json=PERSON)
+        new_person = {
+            "firstName": "name2",
+            "lastName": "name2",
+            "address": "address2",
+            'alternativePhoneNumber': "altPhone2",
+            'mainPhoneNumber': "mainPhone2"
         }
-        res = client.post('/persons', json=person)
+        res = client.put(f'/persons/{res_post.json["id"]}', json=new_person)
+        new_person["id"] = res_post.json["id"]
+        eq_(200, res.status_code)
+        eq_(new_person, res.json)
 
-        res = client.get(f'/persons')
-        ok_(res.json)
-        ok_
+    def test_delete_person(self):
+        res_post = client.post('/persons', json=PERSON)
+        res = client.delete(f"/persons/{res_post.json['id']}")
+        eq_(204, res.status_code)
