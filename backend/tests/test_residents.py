38f55f88954bd8ba0dbc71da4e20a3ba6110b3ed
micamath <michau.mathilde@gmail.com>
2020-05-10 01:18:30 +0200

    additional tests for residents

--- backend/tests/test_residents.py
+++ backend/tests/test_residents.py
@@ -1,14 +1,14 @@
 from backend.models import Person, Resident
 from backend import db
-from config_test import TestApi, client
+from config_test import TestApi, client, is_dict_subset_of_superset
 from nose.tools import eq_, ok_
 
-#   ____  _____ ____ ___ ____  _____ _   _ _____ 
+#   ____  _____ ____ ___ ____  _____ _   _ _____
 #  |  _ \| ____/ ___|_ _|  _ \| ____| \ | |_   _|
-#  | |_) |  _| \___ \| || | | |  _| |  \| | | |  
-#  |  _ <| |___ ___) | || |_| | |___| |\  | | |  
-#  |_| \_\_____|____/___|____/|_____|_| \_| |_|  
-                                               
+#  | |_) |  _| \___ \| || | | |  _| |  \| | | |
+#  |  _ <| |___ ___) | || |_| | |___| |\  | | |
+#  |_| \_\_____|____/___|____/|_____|_| \_| |_|
+
 
 PERSON = {
     "firstName": "name",
@@ -18,7 +18,18 @@ PERSON = {
     'mainPhoneNumber': "mainPhone"
 }
 
-RESIDENT = {}
+RESIDENT = {
+    'birthplace': "birthplace",
+    'psychiatrist_id': None,
+    'cityId': None,
+    'emergencyBag': None,
+    'referringDoctorId': None,
+    'healthMutualId': None,
+    'birthDate': None,
+    'entranceDate': None,
+    'socialWelfareNumber': None,
+}
+
 
 class TestPerson(TestApi):
 
@@ -31,24 +42,26 @@ class TestPerson(TestApi):
         res = client.get('/residents/unknown')
         eq_(404, res.status_code)
 
-    def test_post_persons_no_data(self):
+    def test_post_residents_no_data(self):
         res = client.post('/residents')
         eq_(400, res.status_code)
 
-    def test_post_persons(self):
+    def test_post_residents_unknown_id(self):
         res = client.post('/residents', json={"id": "unknown"})
         eq_(400, res.status_code)
 
-        # res = client.post('/persons', json=PERSON)
+    def test_post_residents(self):
+        res_person = client.post('/persons', json=PERSON)
+        RESIDENT["id"] = PERSON["id"] = res_person.json["id"]
 
-        # eq_(201, res.status_code)
-        # PERSON["id"] = res.json["id"]
-        # eq_(PERSON, res.json)
+        res = client.post('/residents', json=RESIDENT)
+        eq_(201, res.status_code)
 
-        # res = client.get(f'/persons/{PERSON["id"]}')
-        # eq_(200, res.status_code)
-        # eq_(PERSON, res.json)
+        RESIDENT["person"] = PERSON
 
+        res = client.get(f'/residents/{PERSON["id"]}')
+        eq_(200, res.status_code)
+        eq_(True, is_dict_subset_of_superset(res.json, RESIDENT))
     # def test_put_person(self):
     #     res_post = client.post('/persons', json=PERSON)
     #     new_person = {
