1e083619537cebf675c8f1a936abb29bb5c45c47
micamath <michau.mathilde@gmail.com>
2020-05-12 10:18:58 +0200

    add comments and missing test GET ID

--- fiches_urgence/tests/test_person.py
+++ fiches_urgence/tests/test_person.py
@@ -20,6 +20,7 @@ PERSON = {
 
 class TestPerson(TestApi):
 
+    # -------- GET --------
     def test_get_persons(self):
         res = client.get('/persons')
         eq_(200, res.status_code)
@@ -29,21 +30,26 @@ class TestPerson(TestApi):
         res = client.get('/persons/unknown')
         eq_(404, res.status_code)
 
+    def test_get_person_id(self):
+        res = client.post('/persons', json=PERSON)
+        PERSON["id"] = res.json["id"]
+        res = client.get(f'/persons/{PERSON["id"]}')
+        eq_(200, res.status_code)
+        eq_(PERSON, res.json)
+
+    # -------- POST --------
     def test_post_persons_no_data(self):
         res = client.post('/persons')
         eq_(400, res.status_code)
 
     def test_post_persons(self):
         res = client.post('/persons', json=PERSON)
-
         eq_(201, res.status_code)
+
         PERSON["id"] = res.json["id"]
         eq_(PERSON, res.json)
 
-        res = client.get(f'/persons/{PERSON["id"]}')
-        eq_(200, res.status_code)
-        eq_(PERSON, res.json)
-
+    # -------- PUT --------
     def test_put_person(self):
         res_post = client.post('/persons', json=PERSON)
         new_person = {
@@ -58,6 +64,7 @@ class TestPerson(TestApi):
         eq_(200, res.status_code)
         eq_(new_person, res.json)
 
+    # -------- DELETE --------
     def test_delete_person(self):
         res_post = client.post('/persons', json=PERSON)
         res = client.delete(f"/persons/{res_post.json['id']}")
